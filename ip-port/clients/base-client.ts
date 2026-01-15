b57cd6aec7ba863eba9d0625e18c7c8ece77b26b
mcdaddytn <102542695+mcdaddytn@users.noreply.github.com>
2026-01-14 21:00:26 -0600

    Initial setup, wip

--- ip-port/clients/base-client.ts
+++ ip-port/clients/base-client.ts
@@ -117,6 +117,7 @@ export class BaseAPIClient {
         signal: controller.signal,
         headers: {
           'Content-Type': 'application/json',
+          'Accept': 'application/json',
           ...options.headers,
         },
       });
